7d74eaf77174de38b9c9a201f59977f8e2d96d18
gabrielanarvaez <narvaez4@illinois.edu>
2018-09-19 17:20:25 -0500

    New changes.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,18 @@ public class Main {
 	System.out.println("Here");
     }
 }
+public class Count {
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
-/**
- * Retrieve contents from a URL and return them as a string.
- *
- * @param url url to retrieve contents from
- * @return the contents from the url as a string, or an empty string on error
- */
-public static String urlToString(final String url) {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
         Scanner urlScanner;
         try {
             urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
@@ -24,4 +25,6 @@ public static String urlToString(final String url) {
         String contents = urlScanner.useDelimiter("\\A").next();
         urlScanner.close();
         return contents;
-        }
+    }
+
+}
